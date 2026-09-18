091a68f91a641677fbfe6eff8bdc39f3f3d0a4b9
Damianouo <damianouo0@gmail.com>
2026-09-18 15:45:23 +0800

    chore: install eslint-plugin-tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,23 @@
 // @ts-check
+import eslintPluginTailwindcss from "eslint-plugin-tailwindcss";
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   // Your custom configs here
+  eslintPluginTailwindcss.configs['flat/recommended'] || eslintPluginTailwindcss.configs.recommended,
+   {
+    settings: {
+      tailwindcss: {
+        cssConfigPath: "./app/assets/css/main.css",
+      },
+    },
+    rules: {
+      "tailwindcss/classnames-order": "warn",
+      "tailwindcss/no-custom-classname": [
+        "warn",
+        { whitelist: ["custom\\-*"] },
+      ],
+      "tailwindcss/no-contradicting-classname": "warn",
+    },
+  },
 );
